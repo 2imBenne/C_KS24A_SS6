7997d22104cf66d0c1606c3df611640e30268cd9
huylg <lamgiahuy002203@gmail.com>
2024-12-21 13:27:50 +0700

    Hoàn thành bt8

---
+++ Session6-bt8.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+	float deposit,interestRate,tmp;	int month;
+	printf("Moi nhap vao so tien ban dau: ");	
+	scanf("%f",&deposit);	
+	tmp = deposit;
+	printf("Moi nhap vao lai suat thang: ");	
+	scanf("%f%%",&interestRate); 
+	interestRate/=100;
+	printf("Moi nhap vao so thang gui: ");	
+	scanf("%d",&month);
+	
+	float interest;
+	while(month--){
+		interest=deposit*interestRate;
+		deposit += interest;
+	}
+	interest = deposit - tmp;
+	printf("Tien lai: %.3f \nTien nhan duoc: %.3f",interest,deposit);
+}
